2fd9b4c2e84d1a305914b94c3f4e9445ebdfcdbc
Andi Hofmann <andreas-h-online@web.de>
2018-03-28 15:18:29 +0200

    Test

--- src/main/java/ueb01/Palindrom.java
+++ src/main/java/ueb01/Palindrom.java
@@ -4,6 +4,17 @@ class Palindrom {
 	static boolean istPalindrom(String s) {
 		// Verwenden Sie die Klasse CharStackImpl um auf Palindrom zu testen!
 
-		return false;
+        char[] test = s.toCharArray();
+
+        CharStackImpl ele = new CharStackImpl();
+        for(int i = 0; i < test.length / 2; i++){
+            ele.push(test[i]);
+        }
+
+        for(int i = (s.length() + 1) / 2; i < s.length(); i++) {
+            if (test[i] == ele.pop()) {
+                return true;
+            }
+        } return false;
 	}
 }
